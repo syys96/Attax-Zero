bce3b453f2f66d3200ae5c2f7de6288d5f0b8018
syys96 <3359652182@qq.com>
2018-01-12 19:56:47 +0800

    Update board.py

--- board.py
+++ board.py
@@ -1,27 +1,111 @@
 from config import *
 from bitarray import bitarray
+import copy
+
+
+
+near_area = [bitarray('0' * 49)] * 49
+far_area = [bitarray('0' * 49)] * 49
+dx = [-1,-1,-1,0,0,1,1,1,-2,-2,-2,-2,-2,-1,-1,0,0,1,1,2,2,2,2,2]
+dy = [-1,0,1,-1,1,-1,0,1,-2,-1,0,1,2,-2,2,-2,2,-2,2,-2,-1,0,1,2]
+move_to_vertex = {}
+vertex_to_move = {}
+
+num = 0
+
+
+def init_map():
+    for x in range(BOARD_SIZE):
+        for y in range(BOARD_SIZE):
+            for i in range(24):
+                tempx = x + dx[i]
+                tempy = y + dy[i]
+                if 0 <= tempx < BOARD_SIZE and 0 <= tempy < BOARD_SIZE:
+                    global num, move_to_vertex, vertex_to_move
+                    fm = x * BOARD_SIZE + y
+                    to = tempx * BOARD_SIZE + tempy
+                    move_to_vertex[(fm, to)] = num
+                    vertex_to_move[num] = (fm, to)
+                    num += 1
+
+
+def vertex_to_move_fn(vertexs):
+    ret = []
+    for vertex in vertexs:
+        ret.append(vertex_to_move[vertex])
+    return ret
 
 class Board:
     def __init__(self):
-        self.states = [[0, 0]]
         self.states = [[bitarray('0'*49), bitarray('0'*49)]]
-        self.near_area = [bitarray('0'*49)]*49
-        self.far_area = [bitarray('0'*49)]*49
-        self.dx = [-1,-1,-1,0,0,1,1,1,-2,-2,-2,-2,-2,-1,-1,0,0,1,1,2,2,2,2,2]
-        self.dy = [-1,0,1,-1,1,-1,0,1,-2,-1,0,1,2,-2,2,-2,2,-2,2,-2,-1,0,1,2]
         self.step = 0
         self.tomove = BLACK
+        self.players = [BLACK, WHITE]  # player Black and player White
+        init_map()
         self.init_area()
         self.reset_board()
 
-    def reset_board(self):
+    def get_availables(self, color=None, step=None):
+        if color is None:
+            color = self.tomove
+        if step is None:
+            step = self.step
+        moves = self.genmove(color, step)
+        ret = []
+        for var in moves:
+            ret.append(move_to_vertex[var])
+        return ret
+
+    def reset_board(self, start_player=0):
         self.step = 0
         self.tomove = BLACK
         self.states[0][BLACK][0] = '1'
         self.states[0][BLACK][48] = '1'
         self.states[0][WHITE][6] = '1'
         self.states[0][WHITE][42] = '1'
+        self.tomove = self.players[start_player]  # start player
 
+    def full_board(self, step=None):
+        if step is None:
+            step = self.step
+        return (self.states[step][BLACK] | self.states[step][WHITE]).all()
+
+    def procstep(self, move, step=None, color=None):
+        if step is None:
+            step = self.step
+        if color is None:
+            color = self.tomove
+        mystate = copy.deepcopy(self.states[step][color])
+        opstate = copy.deepcopy(self.states[step][1-color])
+
+        fm = move[0]
+        to = move[1]
+        global far_area, near_area
+        if far_area[fm][to]:
+            mystate[fm] = False
+        mystate[to] = True
+        change = near_area[to] & opstate
+        mystate |= change
+        opstate ^= change
+        if color is BLACK:
+            self.states.append([opstate, mystate])
+        else:
+            self.states.append([mystate, opstate])
+
+    def makemove(self, move, step=None, color=None):
+        if step is None:
+            step = self.step
+        if color is None:
+            color = self.tomove
+        self.procstep(move, step, color)
+        self.tomove = 1 - self.tomove
+        self.step += 1
+
+    def delmove(self):
+        if self.step <= 0:
+            return
+        self.tomove = 1 - self.tomove
+        self.step -= 1
 
     def inmap(self, x, y):
         return 0 <= x < BOARD_SIZE and 0 <= y < BOARD_SIZE
@@ -34,11 +118,12 @@ class Board:
         blank_pos = self.get_blank_pos()
         move = []
         for to in blank_pos:
-            nearto = self.near_area[to] & self.states[step][color]
+            global near_area, far_area
+            nearto = near_area[to] & self.states[step][color]
             nearto_pos = self.get_bitarray_pos(nearto)
             if nearto_pos:
                 move.append((nearto_pos[0], to))
-            farto = self.far_area[to] & self.states[step][color]
+            farto = far_area[to] & self.states[step][color]
             farto_pos = self.get_bitarray_pos(farto)
             for frm in farto_pos:
                 move.append((frm, to))
@@ -75,7 +160,7 @@ class Board:
         return ret
 
     def number_to_bitarray(self, number):
-        return bitarray(str(bin(number))[2:])
+        return bitarray(str(bin(number))[2:][::-1])
 
     def get_blank_pos(self, step=None):
         if step is None:
@@ -110,21 +195,24 @@ class Board:
     def init_area(self):
         for x in range(BOARD_SIZE):
             for y in range(BOARD_SIZE):
+                global dx, dy
                 temp = bitarray('0'*49)
                 for i in range(8):
-                    tempx = x + self.dx[i]
-                    tempy = y + self.dy[i]
+                    tempx = x + dx[i]
+                    tempy = y + dy[i]
                     if self.inmap(tempx, tempy):
                         temp[tempx*BOARD_SIZE+tempy] = True
-                self.near_area[x * BOARD_SIZE + y] = temp
+                global near_area
+                near_area[x * BOARD_SIZE + y] = temp
 
                 curr = bitarray('0'*49)
                 for i in range(8, 24):
-                    currx = x + self.dx[i]
-                    curry = y + self.dy[i]
+                    currx = x + dx[i]
+                    curry = y + dy[i]
                     if self.inmap(currx, curry):
                         curr[currx*BOARD_SIZE+curry] = True
-                self.far_area[x * BOARD_SIZE + y] = curr
+                global far_area
+                far_area[x * BOARD_SIZE + y] = curr
 
     def check_pos(self, x, y, color=None, step=None):
         if color is None:
@@ -133,6 +221,24 @@ class Board:
             step = self.step
         return self.states[step][color][x*BOARD_SIZE+y]
 
+    def current_state(self):
+        """
+        return the board state from the perspective of the current player
+        shape: 4*width*height
+        """
+        square_state = np.zeros((4, BOARD_SIZE, BOARD_SIZE))
+        mypos = self.get_side_pos()
+        oppos = self.get_side_pos(color=1-self.tomove)
+        for pos in mypos:
+            square_state[0][pos//BOARD_SIZE][pos%BOARD_SIZE] = 1.0
+        for pos in oppos:
+            square_state[1][pos//BOARD_SIZE][pos%BOARD_SIZE] = 1.0
+        if self.tomove is BLACK:
+            square_state[2][:, :] = 1.0
+        else:
+            square_state[3][:, :] = 1.0
+        return square_state
+
     def print_table(self, step=None):
         if step is None:
             step = self.step
@@ -140,9 +246,9 @@ class Board:
         for x in range(BOARD_SIZE):
             _str += (str(x) + ' ')
             for y in range(BOARD_SIZE):
-                if self.check_pos(x, y, BLACK):
+                if self.check_pos(x, y, BLACK, step):
                     _str += 'B '
-                elif self.check_pos(x, y, WHITE):
+                elif self.check_pos(x, y, WHITE, step):
                     _str += 'W '
                 else:
                     _str += '  '
@@ -151,29 +257,6 @@ class Board:
         _str += '  0 1 2 3 4 5 6\n'
         print(_str)
 
-
 if __name__ == '__main__':
-
-    test = Board()
-    test.print_table()
-    """
-    #print(test.genmove(color=WHITE))
-    for pair in test.genmove(color=WHITE):
-        action = []
-        action.append((pair[0]//BOARD_SIZE, pair[0]%BOARD_SIZE))
-        action.append((pair[1]//BOARD_SIZE, pair[1]%BOARD_SIZE))
-        print(action)
-
-
-
-    #b = bitarray('1010011')
-    #c = bitarray('0000000')
-    #print(b ^ c)
-
-    #c = bitarray('0' * 5)
-    #c[0] = '1'
-    #print(c)
-    #print(c[1])
-    #print(a.index(1, 1, None))
-    """
-    
+    init_map()
+    print(num)
